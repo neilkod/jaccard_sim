6d0829368d676cc52049e44d757fcab3eae7bcc6
Neil Kodner <nkodner@gmail.com>
2012-09-26 23:40:12 -0700

    added rate_pct

--- jaccard.py
+++ jaccard.py
@@ -1,9 +1,9 @@
 from __future__ import division
 import random
 
-USERS = 10000
-ITEMS = 2000
-RATE_PCT = .65
+USERS = 1000
+ITEMS = 200
+RATE_PCT = .45
 
 def jaccard(u1, u2):
   num = len(set(u1.ratings.items()) & set(u2.ratings.items()))
@@ -13,7 +13,7 @@ def jaccard(u1, u2):
 # each user will have a dict of ratings (0,1)
 class user:
   def __init__(self,items=ITEMS):
-    self.ratings = dict(enumerate([random.randint(0,1) for x in range(items)]))
+    self.ratings = dict(enumerate([random.randint(0,1) for x in xrange(items)]))
 
 # have each user rate a certain percentage of items
-users = [user(random.randint(ITEMS * RATE_PCT, ITEMS+1)) for x in range(USERS)]
+users = [user(random.randint(ITEMS * RATE_PCT, ITEMS+1)) for x in xrange(USERS)]
